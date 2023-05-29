795da97d405799cb298180a1bc7a145e7ed1154d
linus071 <linuslim.LL81@gmail.com>
2023-05-28 17:46:44 -0700

    Added content in intro page and HTML files

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/Log.html');
+});
+
+app.get('/Book.html', (req, res) => {
+  res.sendFile(__dirname + '/Book.html');
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Perform login logic here
+  // Validate credentials, check in a database, etc.
+
+  // Example response for successful login
+  if (username === 'admin' && password === 'password') {
+    res.redirect('/Book.html');
+  } else {
+    res.send('Invalid username or password!');
+  }
+});
+
+app.post('/signup', (req, res) => {
+  const { username, password } = req.body;
+
+  // Perform signup logic here
+  // Create a new user, storei n a database, etc.
+
+  // Example response for successful signup
+   res.redirect('/Book.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
