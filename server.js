66a6d39600b09e20639724f8876aa49926c0b011
linus071 <linuslim.LL81@gmail.com>
2023-06-12 00:52:24 -0700

    Added Singup file and mySQl file

--- server.js
+++ server.js
@@ -1,40 +1,74 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mysql = require('mysql2');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
+//Generating ID
+let idCounter = 0;
+
+function generateUniqueId(){
+
+    idCounter++;
+    return idCounter;
+
+}
+
+//Where it first access
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/Intro.html');
 });
 
 
+//Setting up MySQL connection
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'Levi0716@',
+    database: 'user_management',
+})
 
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-
-  // Perform login logic here
-  // Validate credentials, check in a database, etc.
-
-  // Example response for successful login
-  if (username === 'admin' && password === 'password') {
-    res.redirect('/Book.html');
+// Connecting to MySQL
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
   } else {
-    res.send('Invalid username or password!');
+    console.log('Connected to MySQL database');
   }
 });
 
-app.post('/signup', (req, res) => {
-  const { username, password } = req.body;
-
-  // Perform signup logic here
-  // Create a new user, storei n a database, etc.
-
-  // Example response for successful signup
-   res.redirect('/Book.html');
+// Pass the MySQL connection to request handlers
+app.use((req, res, next) => {
+  req.db = connection;
+  next();
 });
 
+
+app.post('/signup', (req, res) => {
+  const id = generateUniqueId(); // Assuming you have a function to generate unique IDs
+  const username = req.body.username;
+  const email = req.body.email;
+  const wechat = req.body.wechat;
+  const password = req.body.password;
+
+  // Insert the signup data into the database
+  const query = `INSERT INTO user_info (id, username, email, wechat, password) VALUES (?, ?, ?, ?, ?)`;
+  connection.query(query, [id, username, email, wechat, password], (error, results) => {
+    if (error) {
+      console.error('Error storing signup data:', error);
+      res.status(500).send('Error storing signup data');
+    } else {
+      console.log('Signup data stored successfully');
+      res.send('Signup successful!');
+    }
+  });
+});
+
+
+
+//Showing which port
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
