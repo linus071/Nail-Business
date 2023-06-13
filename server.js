2244c7b389572d46446c6a9458d802c37afe2e5b
linus071 <linuslim.LL81@gmail.com>
2023-06-13 14:30:50 -0700

    Added Singup file and mySQl file

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ app.get('/', (req, res) => {
 //Setting up MySQL connection
 const connection = mysql.createConnection({
     host: '127.0.0.1',
-    port: '3306',
     user: 'root',
     password: 'Levi0716@',
     database: 'user_management',
