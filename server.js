b9baf6a446125859da0e4f0311232ecc88a2ac29
linus071 <linuslim.LL81@gmail.com>
2023-06-13 16:44:40 -0700

    Changes DB

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const CyclicDb = require('@cyclic.sh/dynamodb');
+const mysql = require('mysql2');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
@@ -9,64 +9,79 @@ app.use(express.static('public'));
 //Generating ID
 let idCounter = 0;
 
+// Retrieve the last assigned ID from the database and set idCounter accordingly
+function fetchLastAssignedId() {
+  const query = "SELECT MAX(id) AS lastId FROM user_info";
+  connection.query(query, (error, results) => {
+    if (error) {
+      console.error('Error fetching last assigned ID:', error);
+    } else {
+      idCounter = results[0].lastId || 0;
+      console.log('Last assigned ID:', idCounter);
+    }
+  });
+}
+
 //Where it first access
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/Intro.html');
 });
 
-// Initializing CyclicDB
-const db = CyclicDb('weary-undershirt-bearCyclicDB');
 
-// Fetch the last assigned ID from CyclicDB and set idCounter accordingly
-async function fetchLastAssignedId() {
-  try {
-    const result = await db.query('user_info').orderBy('id', 'desc').limit(1).fetch();
-    if (result.length > 0) {
-      idCounter = result[0].id;
-      console.log('Last assigned ID:', idCounter);
-    }
-  } catch (error) {
-    console.error('Error fetching last assigned ID:', error);
+//Setting up MySQL connection
+const connection = mysql.createConnection({
+    host: 'aws.connect.psdb.cloud',
+    user: '9mfk6i0iaauvc1xc5ain',
+    password: 'pscale_pw_2A2sYlGzwg8A4yPb52wg9ThiJPLvNO84yNDH4gDxsz1',
+    database: 'ubc_nails_info',
+})
+
+// Connecting to MySQL
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+//    process.exit(1);
+  } else {
+    console.log('Connected to MySQL database');
+    fetchLastAssignedId();
   }
+});
+
+function generateUniqueId(){
+
+    idCounter++;
+    return idCounter;
+
 }
 
-// Generating unique ID
-function generateUniqueId() {
-  idCounter++;
-  return idCounter.toString();
-}
-
-// Pass the CyclicDB instance to request handlers
+// Pass the MySQL connection to request handlers
 app.use((req, res, next) => {
-  req.db = db;
+  req.db = connection;
   next();
 });
 
-app.post('/signup', async (req, res) => {
+
+app.post('/signup', (req, res) => {
   const id = generateUniqueId(); // Assuming you have a function to generate unique IDs
   const username = req.body.username;
   const email = req.body.email;
   const wechat = req.body.wechat;
   const password = req.body.password;
 
-  const users = db.collection('users'); // Use your desired collection name here
-
-  // create an item in the collection with the key as the generated ID
-  try {
-    await users.set(id.toString(), {
-      username,
-      email,
-      wechat,
-      password
-    });
-
-    console.log('Signup data stored successfully');
-    res.send('Signup successful!');
-  } catch (error) {
-    console.error('Error storing signup data:', error);
-    res.status(500).send('Error storing signup data');
-  }
+  // Insert the signup data into the database
+  const query = `INSERT INTO user_info (id, username, email, wechat, password) VALUES (?, ?, ?, ?, ?)`;
+  connection.query(query, [id, username, email, wechat, password], (error, results) => {
+    if (error) {
+      console.error('Error storing signup data:', error);
+      res.status(500).send('Error storing signup data');
+    } else {
+      console.log('Signup data stored successfully');
+      res.send('Signup successful!');
+    }
+  });
 });
+
+
 
 //Showing which port
 app.listen(port, () => {
