8c1ef728493dab3400208661a2f22b511c5ce460
linus071 <linuslim.LL81@gmail.com>
2023-06-14 01:53:34 -0700

    SIGNUP SUCCESSFULL!!!!

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const mysql = require('mysql2');
-const fs = require('fs');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
